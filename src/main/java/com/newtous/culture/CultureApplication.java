8e9ace385c353af8ee5c89d781a878c4192f9636
Kane Wang <wanglikai@gmail.com>
2018-11-02 22:39:17 -0700

    initail

---
+++ src/main/java/com/newtous/culture/CultureApplication.java
@@ -0,0 +1,12 @@
+package com.newtous.culture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultureApplication.class, args);
+	}
+}
