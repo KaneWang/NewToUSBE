841ae9f2cf5043f30f11804d4764536da55aa2e7
Kane Wang <wanglikai@gmail.com>
2018-11-02 23:17:28 -0700

    adding restful api support

--- src/main/java/com/newtous/culture/HelloWorldApplication.java
+++ src/main/java/com/newtous/culture/HelloWorldApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CultureApplication {
+public class HelloWorldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CultureApplication.class, args);
+		SpringApplication.run(HelloWorldApplication.class, args);
 	}
+
 }
