5b468ecca02a67a399f9bbc733ffa8d7ae1913e0
Kane Wang <wanglikai@gmail.com>
2018-11-23 23:01:42 -0800

    adding app engine config for g cloud

--- src/main/java/com/newtous/culture/HelloWorldApplication.java
+++ src/main/java/com/newtous/culture/HelloWorldApplication.java
@@ -2,12 +2,18 @@ package com.newtous.culture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
+    @GetMapping("/")
+    public String hello() {
+        return "hello world!";
+    }
 }
